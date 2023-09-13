530e1a38d0475c2c32ac1050d939463b70c4379d
s-alv <santia95@hotmail.com>
2023-09-13 17:49:01 -0300

    created frontend

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseURL: "https://employee-management-app-j75t.onrender.com",
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  baseURL: "https://employee-management-app-j75t.onrender.com",
+};
